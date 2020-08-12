8b4519ff7629c0d14f243a2ec80ede5bd64c6391
kascodingNL <kas@kasnetwork.nl>
2020-08-12 17:06:13 +0200

    Added benchmarks, added TryParse benchmark

--- Parser2.0/Program.cs
+++ Parser2.0/Program.cs
@@ -55,6 +55,10 @@ namespace CustomIntParser
 
         public static void TestJob(int iterations)
         {
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("");
+
             Stopwatch totalTime = new Stopwatch();
             totalTime.Start();
 
@@ -64,8 +68,14 @@ namespace CustomIntParser
             {
                 int customParsed = IntParseFast("95858");
             }
-            Console.WriteLine("Test 1 took " + watch1.Elapsed);
             watch1.Stop();
+
+            double ns = 1000000000.0 * (double)watch1.ElapsedTicks / Stopwatch.Frequency;
+            double ms = ns / 1000000.0;
+
+            Console.WriteLine("Test 1 took " + ns + " nanoseconds(or " + ms + "ms).");
+
+            Console.WriteLine("");
 
             Stopwatch watch2 = new Stopwatch();
             watch2.Start();
@@ -75,8 +85,27 @@ namespace CustomIntParser
                 int parsed = 0;
                 parsed = int.Parse("95858");
             }
-            Console.WriteLine("Test 2 took " + watch2.Elapsed);
+
+            double ns1 = 1000000000.0 * (double)watch2.ElapsedTicks / Stopwatch.Frequency;
+            double ms1 = ns1 / 1000000.0;
+            Console.WriteLine("Test 2 took " + ns1 + " nanoseconds(or " + ms1 +"ms).");
             watch2.Stop();
+
+            Console.WriteLine("");
+
+            Stopwatch watch3 = new Stopwatch();
+            watch3.Start();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                int parsed = 0;
+                int.TryParse("95858", out parsed);
+            }
+
+            double ns2 = 1000000000.0 * (double)watch3.ElapsedTicks / Stopwatch.Frequency;
+            double ms2 = ns2 / 1000000.0;
+            Console.WriteLine("Test 3 took " + ns2 + " nanoseconds(or " + ms2 + "ms).");
+            watch3.Stop();
 
             totalTime.Stop();
             Console.WriteLine("Total test took " + totalTime.Elapsed);
